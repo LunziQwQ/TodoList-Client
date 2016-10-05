83b50acab98abe8dfcd3292d6585f4fe5c88e067
LunziQwQ <me@lunzi.space>
2016-10-05 18:34:29 +0800

    First sync

---
+++ TodoList/Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace TodoList {
+    partial class mainForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if(disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.btn_close = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_close
+            // 
+            this.btn_close.AccessibleRole = System.Windows.Forms.AccessibleRole.None;
+            this.btn_close.BackColor = System.Drawing.Color.Transparent;
+            this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_close.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_close.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.btn_close.Location = new System.Drawing.Point(281, -4);
+            this.btn_close.Name = "btn_close";
+            this.btn_close.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.btn_close.Size = new System.Drawing.Size(51, 32);
+            this.btn_close.TabIndex = 0;
+            this.btn_close.Text = "×";
+            this.btn_close.UseVisualStyleBackColor = false;
+            this.btn_close.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(328, 411);
+            this.Controls.Add(this.btn_close);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.ForeColor = System.Drawing.Color.White;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "mainForm";
+            this.Text = "TodoList";
+            this.Load += new System.EventHandler(this.mainForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_close;
+    }
+}
+
