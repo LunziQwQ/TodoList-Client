606c1e091ad08b5d309aa790d5f362b13be5665c
LunziQwQ <me@lunzi.space>
2016-10-11 20:28:23 +0800

    Sync

--- TodoList/mainForm.Designer.cs
+++ TodoList/mainForm.Designer.cs
@@ -24,6 +24,7 @@
         /// </summary>
         private void InitializeComponent() {
             this.btn_close = new System.Windows.Forms.Button();
+            this.TitleLabel = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btn_close
@@ -32,36 +33,55 @@
             this.btn_close.BackColor = System.Drawing.Color.Transparent;
             this.btn_close.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
             this.btn_close.Font = new System.Drawing.Font("Consolas", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btn_close.ForeColor = System.Drawing.SystemColors.GrayText;
-            this.btn_close.Location = new System.Drawing.Point(281, -4);
+            this.btn_close.ForeColor = System.Drawing.SystemColors.ButtonShadow;
+            this.btn_close.Location = new System.Drawing.Point(360, -5);
+            this.btn_close.Margin = new System.Windows.Forms.Padding(4);
             this.btn_close.Name = "btn_close";
             this.btn_close.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.btn_close.Size = new System.Drawing.Size(51, 32);
+            this.btn_close.Size = new System.Drawing.Size(83, 41);
             this.btn_close.TabIndex = 0;
             this.btn_close.Text = "×";
             this.btn_close.UseVisualStyleBackColor = false;
-            this.btn_close.Click += new System.EventHandler(this.button1_Click);
+            this.btn_close.Click += new System.EventHandler(this.btn_close_Click);
+            this.btn_close.MouseEnter += new System.EventHandler(this.btn_close_MouseEnter);
+            this.btn_close.MouseLeave += new System.EventHandler(this.btn_close_MouseLeave);
+            // 
+            // TitleLabel
+            // 
+            this.TitleLabel.AutoSize = true;
+            this.TitleLabel.BackColor = System.Drawing.Color.Transparent;
+            this.TitleLabel.Font = new System.Drawing.Font("Consolas", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TitleLabel.ForeColor = System.Drawing.Color.DimGray;
+            this.TitleLabel.Location = new System.Drawing.Point(13, 13);
+            this.TitleLabel.Name = "TitleLabel";
+            this.TitleLabel.Size = new System.Drawing.Size(98, 23);
+            this.TitleLabel.TabIndex = 1;
+            this.TitleLabel.Text = "TodoList";
             // 
             // mainForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.White;
-            this.ClientSize = new System.Drawing.Size(328, 411);
+            this.ClientSize = new System.Drawing.Size(437, 548);
+            this.Controls.Add(this.TitleLabel);
             this.Controls.Add(this.btn_close);
             this.Cursor = System.Windows.Forms.Cursors.Default;
             this.ForeColor = System.Drawing.Color.White;
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "mainForm";
+            this.Opacity = 0.95D;
             this.Text = "TodoList";
-            this.Load += new System.EventHandler(this.mainForm_Load);
             this.ResumeLayout(false);
-
+            this.PerformLayout();
+            this.MouseDown = new System.EventHandler(this.mainForm_MouseDown);
         }
 
         #endregion
 
         private System.Windows.Forms.Button btn_close;
+        private System.Windows.Forms.Label TitleLabel;
     }
 }
 
---
+++ TodoList/mainForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TodoList {
+    public partial class mainForm : Form {
+        public mainForm() {
+            InitializeComponent();
+        }
+
+        private void btn_close_Click(object sender, EventArgs e) {
+            Close();
+        }
+
+        private void btn_close_MouseEnter(object sender, EventArgs e) {
+            btn_close.BackColor = Color.OrangeRed;
+        }
+
+        private void btn_close_MouseLeave(object sender, EventArgs e) {
+            btn_close.BackColor = Color.Transparent;
+        }
+
+        private void mainForm_MouseDown(object sender, EventArgs e) {
+
+        }
+        
+    }
+}
