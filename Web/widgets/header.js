2a1b56c066c71c8ff635b2c43caf693b0d74801a
xxsgithub <wxxzhuang@gmail.com>
2016-11-01 02:16:20 +0800

    Add the header...

--- Web/widgets/header.js
+++ Web/widgets/header.js
@@ -1,3 +1,13 @@
 /**
  * Created by XX on 2016/11/1.
  */
+
+function search_focus() {
+    $(".search>div").toggleClass('active');
+}
+
+$(document).ready(function () {
+    $(".search input").focusin(search_focus);
+    $(".search input").focusout(search_focus);
+
+});
